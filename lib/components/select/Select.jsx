f64d4ca0d9de4b500c9d6c69fc5efd249a68afec
Rémi Michel <remi.michel38@gmail.com>
2025-03-18 20:07:52 +0100

    feat(): first setup of select component

---
+++ lib/components/select/Select.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './select.css'
+
+const Select = ({options, onChange, selected}) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const onOpen = (e) => {
+        e.preventDefault()
+        setIsOpen(!isOpen)
+    }
+
+    const onClick = (option) => {
+        setIsOpen(false)
+        onChange(option)
+    }
+
+    return <div className="select-container">
+        <button onClick={onOpen}>{selected.label}</button>
+        <ul className={`select-options ${isOpen ? 'show' : ''}`}>
+            {options.map((option) => <li 
+                key={option.value} 
+                className={`${option.value === selected.value ? 'selected' : ''}`}
+                onClick={() => onClick(option)}>{option.label}
+            </li>)}
+        </ul>
+    </div>
+}
+
+export default Select
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), libInjectCss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.js'),
+      formats: ['es']
+    }
+  }
+})
